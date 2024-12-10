6f292c14a166b2103fdc9319312961c011fd4b43
Roman Liukevich <Raman.Liukevich@ringieraxelspringer.pl>
2024-12-10 17:49:57 +0100

    initial commit

--- vorobei_4/ContentView.swift
+++ vorobei_4/ContentView.swift
@@ -2,20 +2,74 @@
 //  ContentView.swift
 //  vorobei_4
 //
-//  Created by Raman Liukevich on 10/12/2024.
+//  Created by Raman Liukevich on 05/12/2024.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var onTap: Bool = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+        HStack(spacing: 0) {
+            Button {
+                withAnimation(.snappy) {
+                    onTap.toggle()
+                } completion: {
+                    onTap = false
+                }
+
+            } label: {
+                HStack(spacing: 0) {
+
+                    Image(systemName: "play.fill")
+                        .resizable()
+                        .frame(width: onTap ? 50 : 0, height: onTap ? 50 : 0)
+
+                    Image(systemName: "play.fill")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+
+                    Image(systemName: "play.fill")
+                        .resizable()
+                        .frame(width: onTap ? 0.5 : 50, height: onTap ? 0.5 : 50)
+
+                }
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            }
+            .buttonStyle(RoundButton())
         }
-        .padding()
+    }
+}
+
+struct RoundButton: ButtonStyle {
+    @State private var showBackground = false
+    @State private var circleScale: CGFloat = 1.0
+
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack {
+            Circle()
+                .fill(Color.black.opacity(0.2))
+                .frame(width: 135, height: 135)
+                .scaleEffect(circleScale)
+                .animation(.easeOut(duration: 0.22), value: circleScale)
+                .opacity(showBackground ? 1 : 0)
+                .animation(.easeOut(duration: 0.22), value: showBackground)
+
+            configuration.label
+                .padding()
+                .scaleEffect(configuration.isPressed ? 0.6 : 1)
+                .animation(.easeOut(duration: 0.22), value: configuration.isPressed)
+                .onChange(of: configuration.isPressed) { oldValue, isPressed in
+                    if isPressed {
+                        showBackground = true
+                        circleScale = 0.8
+                    } else {
+                        showBackground = false
+                        circleScale = 1.0
+                    }
+                }
+        }
     }
 }
 
